ade6de566c286efc4b2abcc2325b5516860caf5c
Mahdi <mahdi7192@gmail.com>
2023-03-04 00:21:05 +0330

    order list title

--- packages/app/src/app/pages/orders/orders.module.ts
+++ packages/app/src/app/pages/orders/orders.module.ts
@@ -8,9 +8,20 @@ import { MatCardModule } from '@angular/material/card';
 import { SharedModule } from '../../shared/shared.module';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [OrderListComponent, OrderCardComponent],
-  imports: [CommonModule, RouterModule.forChild(OrdersRoutes), SharedModule, MatCardModule, MatChipsModule, MatDividerModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(OrdersRoutes),
+    SharedModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
 })
 export class OrdersModule {}
