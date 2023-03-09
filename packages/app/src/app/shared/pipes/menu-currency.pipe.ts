eb29db3c69b32d2b11465260cdd2cff7f2bd9965
Mahdi <mahdi7192@gmail.com>
2023-03-09 13:03:13 +0330

    free

--- packages/app/src/app/shared/pipes/menu-currency.pipe.ts
+++ packages/app/src/app/shared/pipes/menu-currency.pipe.ts
@@ -10,12 +10,17 @@ export class MenuCurrencyPipe implements PipeTransform {
   constructor(
     private menuService: MenuService,
     private decimal: DecimalPipe,
+    private translateService: TranslateService,
   ) {}
   transform(value: any, digitsInfo?: string): string {
     const currency = this.menuService.menu?.currency || 'تومان';
     try {
-      if (value != '' && value != null && !isNaN(value)) {
-        return this.decimal.transform(value, digitsInfo || '1.0-0') + ' ' + currency;
+      if (value !== '' && value != null && !isNaN(value)) {
+        if (value != 0) {
+          return this.decimal.transform(value, digitsInfo || '1.0-0') + ' ' + currency;
+        } else {
+          return this.translateService.instant('app.free');
+        }
       } else {
         return value || '' + currency;
       }
--- packages/panel/src/app/shared/pipes/menu-currency.pipe.ts
+++ packages/panel/src/app/shared/pipes/menu-currency.pipe.ts
@@ -2,24 +2,27 @@ import { DecimalPipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { MenuService } from '../../core/services/menu.service';
-import { ShopService } from '../../core/services/shop.service';
 
 @Pipe({
   name: 'menuCurrency',
 })
 export class MenuCurrencyPipe implements PipeTransform {
   constructor(
-    private shopService: ShopService,
+    private menuService: MenuService,
     private decimal: DecimalPipe,
-    private translate: TranslateService
+    private translateService: TranslateService
   ) {}
   transform(value: any, digitsInfo?: string): string {
-    const currency = this.shopService.shop?.menu?.currency || 'تومان';
+    const currency = this.menuService.menu?.currency || 'تومان';
     try {
-      if (value != '' && value != null && !isNaN(value)) {
-        return this.decimal.transform(value, digitsInfo || '1.0-0') + ' ' + currency;
+      if (value !== '' && value != null && !isNaN(value)) {
+        if (value != 0) {
+          return this.decimal.transform(value, digitsInfo || '1.0-0') + ' ' + currency;
+        } else {
+          return this.translateService.instant('app.free');
+        }
       } else {
-        return value || currency;
+        return value || '' + currency;
       }
     } catch (error) {
       return value;
