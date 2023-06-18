73958ee8d4b20106cda89f257333e3556d32f140
Mahdi <mahdi7192@gmail.com>
2023-06-18 11:11:23 +0330

    printer help google doc

--- packages/panel/src/environments/environment.ts
+++ packages/panel/src/environments/environment.ts
@@ -11,9 +11,9 @@ export const environment = {
   localPrintServiceUrl: 'http://127.0.0.1:5072/',
   bucketUrl: 'https://menno.storage.iran.liara.space',
   printerZipUrl: 'https://menno.storage.iran.liara.space/assets/menno-pro-printer%201.1.0.zip',
-  netFrameworkUrl: 'https://dl2.soft98.ir/soft/n/NET.Framework.4.7.2.rar',
   alopeykBaseUrl: 'https://api.alopeyk.com/api/v2',
   updateDocLink: 'https://docs.google.com/document/d/1mZ5SwfnZIlxR7HeOdMwpPXCi_Dln_wGaQdf4hShSELE/edit?usp=sharing',
+  printerDocLink: 'https://docs.google.com/document/d/1s_Ua3ZQDzjj9s8iYFgn8kpOFHuZ70pdmaBaERkubxRY/edit?usp=sharing',
   sizpayDocLink: 'https://docs.google.com/document/d/1jV8uOBQLz7qQeuzx2F__oe8gznnW1GaDikSWHv-BJPk/edit?usp=sharing',
 };
 
