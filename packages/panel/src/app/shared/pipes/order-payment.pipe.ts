bbc59c50d325a0c4746bac3d9fbe5f9946310fd9
Mahdi <mahdi7192@gmail.com>
2023-07-17 16:12:03 +0330

    pos responsive

--- packages/panel/src/app/shared/pipes/order-payment.pipe.ts
+++ packages/panel/src/app/shared/pipes/order-payment.pipe.ts
@@ -1,19 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { OrderPaymentType } from '@menno/types';
 import { TranslateService } from '@ngx-translate/core';
+import { ShopService } from '../../core/services/shop.service';
 
 @Pipe({
   name: 'orderPayment',
 })
 export class OrderPaymentPipe implements PipeTransform {
-  constructor(private translate: TranslateService) {}
+  constructor(private translate: TranslateService, private shopService: ShopService) {}
 
-  transform(type: OrderPaymentType): unknown {
+  transform(type: OrderPaymentType, posPayed?: number[]): unknown {
+    const posIndex = posPayed?.findIndex((x) => x > 0);
+    const pos =
+      posIndex && this.shopService.shop?.details?.poses && this.shopService.shop?.details?.poses[posIndex];
     switch (type) {
       case OrderPaymentType.Online:
         return this.translate.instant('orderPaymentType.online');
       case OrderPaymentType.Cash:
-        return this.translate.instant('orderPaymentType.cash');
+        return pos || this.translate.instant('orderPaymentType.cash');
       case OrderPaymentType.ClubWallet:
         return this.translate.instant('orderPaymentType.clubWallet');
       default:
