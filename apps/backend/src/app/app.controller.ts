8bbb4409a7f1b478577d036f9237090714d741c8
Mahdi <mahdi7192@gmail.com>
2023-09-24 12:25:12 +0330

    ng sw

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { Public } from './auth/public.decorator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Shop } from '@menno/types';
+import { Repository } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, @InjectRepository(Shop)
+  private shopsRepository: Repository<Shop>) {}
 
   @Public()
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Public()
+  @Get('health-check')
+  async healthCheck() {
+    const shops = await this.shopsRepository.count();
+    return HttpStatus.OK;
   }
 }
