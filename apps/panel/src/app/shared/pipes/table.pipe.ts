9b62ed78c6d3fe17c425db6b73ef7179b129decd
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-01-02 12:36:35 +0330

    table bug fixed

--- apps/panel/src/app/shared/pipes/table.pipe.ts
+++ apps/panel/src/app/shared/pipes/table.pipe.ts
@@ -5,18 +5,19 @@ import { MenuService } from '../../core/services/menu.service';
 import { ShopService } from '../../core/services/shop.service';
 
 @Pipe({
-  name: 'table',
+  name: 'shopTable',
 })
 export class TablePipe implements PipeTransform {
   constructor(private shopService: ShopService) {}
   transform(value?: string): string {
+    let res = ''
     if (value) {
-      let res = value;
+      res += value;
       const table = this.shopService.shop?.details?.tables?.find((x) => x.code === value);
-      if (table) {
+      if (table?.title) {
         res += ` (${table.title})`;
       }
     }
-    return '';
+    return res;
   }
 }
