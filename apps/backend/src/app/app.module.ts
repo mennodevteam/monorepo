e41a7c97d018b5f15e6c01251787ef5d5f7f7ade
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-01-02 15:29:28 +0330

    db connection pool

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -29,7 +29,6 @@ import { AppRedirectController } from './app-redirect.controller';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './core/http-exception.filter';
 import { HealthModule } from './health/health.module';
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -42,9 +41,20 @@ import { HealthModule } from './health/health.module';
       synchronize: true,
       keepConnectionAlive: true,
       autoLoadEntities: true,
+
       extra: {
         extensions: ['pg_stat_statements'],
+        pool: {
+          max: process.env.DB_POOL_MAX ? Number(process.env.DB_POOL_MAX) : 10,
+          min:  process.env.DB_POOL_MAX ? Number(process.env.DB_POOL_MIN) : 2,
+          acquire:  process.env.DB_POOL_MAX ? Number(process.env.DB_POOL_ACQUIRE) : 30000,
+          idle:  process.env.DB_POOL_MAX ? Number(process.env.DB_POOL_IDLE) : 10000,
+        },
       },
+      poolSize: process.env.DB_POOL_MAX ? Number(process.env.DB_POOL_MAX) : 10,
+      poolErrorHandler: (error: any) => {
+        console.error('pool error handler', error)
+      }
     }),
     HealthModule,
     SchemasModule,
