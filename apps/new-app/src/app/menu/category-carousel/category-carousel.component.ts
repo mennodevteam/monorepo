84889642c033414a69c8667de3364361e7961102
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-07-09 14:58:10 +0330

    query selector selected cateogry

--- apps/new-app/src/app/menu/category-carousel/category-carousel.component.ts
+++ apps/new-app/src/app/menu/category-carousel/category-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { AfterViewInit, Component, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Menu } from '@menno/types';
 import { COMMON } from '../../common';
@@ -11,9 +11,33 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './category-carousel.component.html',
   styleUrl: './category-carousel.component.scss',
 })
-export class CategoryCarouselComponent {
+export class CategoryCarouselComponent implements AfterViewInit {
   @Input() menu: Menu;
   @Input() selectedIndex = signal(0);
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        const intersected = entries.filter((elem) => elem.isIntersecting === true)[0];
+        if (intersected) {
+          const targets = document.querySelectorAll('section.category-section h2');
+          targets.forEach((t, index) => {
+            if (t === intersected.target) this.selectedIndex.set(index);
+          });
+        }
+      },
+      {
+        rootMargin: '-200px',
+      }
+    );
+
+    const targets = document.querySelectorAll('section.category-section h2');
+    targets.forEach((element) => {
+      if (element) observer.observe(element);
+    });
+  }
 
   selectCategory(index: number) {
     this.selectedIndex.set(index);
