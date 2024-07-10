615908f931e4b94efbf1aa57a15ffba7831b9ab9
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-07-10 12:03:14 +0330

    card view

--- apps/new-app/src/app/menu/category-section/category-section.component.ts
+++ apps/new-app/src/app/menu/category-section/category-section.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCategory } from '@menno/types';
 import { COMMON } from '../../common';
-import { ProductListViewComponent } from "../product/product-list-view/product-list-view.component";
-import { ProductGridViewComponent } from "../product/product-grid-view/product-grid-view.component";
+import { ProductListViewComponent } from "../product-list/product-list-view/product-list-view.component";
+import { ProductGridViewComponent } from "../product-list/product-grid-view/product-grid-view.component";
+import { ProductCardViewComponent } from "../product-list/product-card-view/product-card-view.component";
 
 @Component({
   selector: 'app-category-section',
   standalone: true,
-  imports: [CommonModule, COMMON, ProductListViewComponent, ProductGridViewComponent],
+  imports: [CommonModule, COMMON, ProductListViewComponent, ProductGridViewComponent, ProductCardViewComponent],
   templateUrl: './category-section.component.html',
   styleUrl: './category-section.component.scss',
 })
---
+++ apps/new-app/src/app/menu/product-list/product-card-view/product-card-view.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { COMMON } from '../../../common';
+import { Product, ProductCategory } from '@menno/types';
+import { MatCardModule } from '@angular/material/card'
+import { QuantitySelectorComponent } from "../../../common/components/quantity-selector/quantity-selector.component";
+
+@Component({
+  selector: 'app-product-card-view',
+  standalone: true,
+  imports: [CommonModule, COMMON, MatCardModule, QuantitySelectorComponent],
+  templateUrl: './product-card-view.component.html',
+  styleUrl: './product-card-view.component.scss',
+})
+export class ProductCardViewComponent {
+  Product = Product;
+  @Input() category: ProductCategory;
+}
