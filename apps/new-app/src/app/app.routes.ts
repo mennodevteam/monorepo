b4427411de5c52c1011bc0a526f5f89de80feb0a
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-07-10 13:59:19 +0330

    intro complete

--- apps/new-app/src/app/app.routes.ts
+++ apps/new-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Route } from '@angular/router';
 import { shopRoutes } from './shop/shop.routes';
 import { menuRoutes } from './menu/menu.routes';
 import { shopResolver, translateActivator, userResolver } from './core';
+import { ShellComponent } from './shell/shell.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
+    component: ShellComponent,
     resolve: {
       user: userResolver,
       shop: shopResolver,
