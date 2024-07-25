3b650f91f8a2b2af0cf95e2ec3062bbf3b54f327
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-07-25 20:33:07 +0330

    iranyekan font

--- apps/new-app/src/app/common/pipes/menu-currency.pipe.ts
+++ apps/new-app/src/app/common/pipes/menu-currency.pipe.ts
@@ -10,11 +10,11 @@ import { MenuService } from '../../core/services/menu.service';
 export class MenuCurrencyPipe implements PipeTransform {
   constructor(private menuService: MenuService, private translateService: TranslateService) {}
   transform(value: any, digitsInfo?: string): string {
-    const currency = this.menuService.menu().currency || 'تومان';
+    const currency = this.menuService.menu().currency || this.translateService.instant('app.toman');
     try {
       if (value !== '' && value != null && !isNaN(value)) {
         if (value != 0) {
-          return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' ' + currency;
+          return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.') + ' ' + currency;
         } else {
           return this.translateService.instant('app.free');
         }
