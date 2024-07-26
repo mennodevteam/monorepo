2ac24ee2772aa051dcb7cee8f3e2f285d703ce37
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-07-26 21:23:14 +0330

    heic photo update

--- apps/panel/src/app/shared/dialogs/image-cropper-dialog/image-cropper-dialog.component.ts
+++ apps/panel/src/app/shared/dialogs/image-cropper-dialog/image-cropper-dialog.component.ts
@@ -1,14 +1,7 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Inject,
-  OnInit,
-  Optional,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, Optional, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { base64ToFile, ImageCroppedEvent } from 'ngx-image-cropper';
+import heic2any from 'heic2any';
 
 @Component({
   selector: 'app-image-cropper-dialog',
@@ -22,7 +15,7 @@ export class ImageCropperDialogComponent implements AfterViewInit {
 
   constructor(
     @Inject(MAT_DIALOG_DATA) @Optional() public data: any,
-    public dialogRef: MatDialogRef<any>
+    public dialogRef: MatDialogRef<any>,
   ) {}
 
   ngAfterViewInit(): void {
@@ -31,8 +24,35 @@ export class ImageCropperDialogComponent implements AfterViewInit {
     }, 500);
   }
 
-  fileChangeEvent(event: any): void {
-    this.imageChangedEvent = event;
+  async fileChangeEvent(event: any) {
+    const file = event.target.files[0];
+    if (file.type.search('heic') >= 0) {
+      const convertedFile: Blob = (await heic2any({
+        blob: file,
+        toType: 'image/jpeg',
+        quality: 1,
+      })) as Blob;
+
+      let list = new DataTransfer();
+      list.items.add(new File([convertedFile], 'file.jpeg', { type: 'image/jpeg' }));
+
+      const newInput = document.createElement('input');
+      newInput.onchange = (ev) => {
+        console.log(ev);
+      };
+      newInput.files = list.files;
+
+      // this.fileInput.nativeElement.value = convertedFile;
+      // // const newFile = new File([convertedFile], 'img.jpg', { type: 'image/jpeg', lastModified: new Date().getTime() });
+      // // let container = new DataTransfer();
+      // event.target.files = [convertedFile];
+      // this.imageChangedEvent = list;
+      this.fileInput.nativeElement.files = list.files;
+      console.log(this.fileInput.nativeElement.files[0]);
+      this.imageChangedEvent = event;
+    } else {
+      this.imageChangedEvent = event;
+    }
   }
   imageCropped(event: ImageCroppedEvent) {
     if (event.base64) {
