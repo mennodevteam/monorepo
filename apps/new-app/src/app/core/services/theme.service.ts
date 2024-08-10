ae2eec46a57483c4d085d6f032cbb205273cd528
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-08-10 12:01:09 +0330

    some issue chages

--- apps/new-app/src/app/core/services/theme.service.ts
+++ apps/new-app/src/app/core/services/theme.service.ts
@@ -90,28 +90,32 @@ export class ThemeService {
   }
 
   private createCustomProperties(schemes: any) {
-    let sheet = (globalThis as any)['material-tokens-class'];
+    try {
+      let sheet = (globalThis as any)['material-tokens-class'];
 
-    if (!sheet) {
-      sheet = new CSSStyleSheet();
-      (globalThis as any)['material-tokens-class'] = sheet;
-      document.adoptedStyleSheets.push(sheet);
-    }
-
-    let tokenClassString = ``;
-    for (const key in schemes) {
-      if (Object.prototype.hasOwnProperty.call(schemes, key)) {
-        const keyText = key
-          .replace(/([a-z])([A-Z])/g, '$1-$2')
-          .replace(/[\s_]+/g, '-')
-          .toLowerCase();
-
-        const value = hexFromArgb(schemes[key]);
-        document.body.style.setProperty(`--sys-${keyText}`, value);
-        tokenClassString += `.${keyText}-text{color:${value} !important}.${keyText}-background{background-color:${value} !important}`;
+      if (!sheet) {
+        sheet = new CSSStyleSheet();
+        (globalThis as any)['material-tokens-class'] = sheet;
+        document.adoptedStyleSheets.push(sheet);
       }
+
+      let tokenClassString = ``;
+      for (const key in schemes) {
+        if (Object.prototype.hasOwnProperty.call(schemes, key)) {
+          const keyText = key
+            .replace(/([a-z])([A-Z])/g, '$1-$2')
+            .replace(/[\s_]+/g, '-')
+            .toLowerCase();
+
+          const value = hexFromArgb(schemes[key]);
+          document.body.style.setProperty(`--sys-${keyText}`, value);
+          tokenClassString += `.${keyText}-text{color:${value} !important}.${keyText}-background{background-color:${value} !important}`;
+        }
+      }
+      sheet.replaceSync(tokenClassString);
+    } catch (error) {
+      console.log(error);
     }
-    sheet.replaceSync(tokenClassString);
   }
 
   private getLuminance(hexColor: string): number {
