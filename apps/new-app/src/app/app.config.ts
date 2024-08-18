b482318444e065c91afc26619bb45f68ee2472f5
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-08-18 11:44:13 +0330

    performance

--- apps/new-app/src/app/app.config.ts
+++ apps/new-app/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { apiInterceptorProvider, provideTranslation } from './core';
@@ -9,7 +9,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimationsAsync(),
-    provideRouter(appRoutes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
+    provideRouter(
+      appRoutes,
+      withPreloading(PreloadAllModules),
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
+    ),
     provideHttpClient(withInterceptorsFromDi()),
     apiInterceptorProvider(),
     provideTranslation(),
--- apps/new-app/src/app/cart/cart.component.ts
+++ apps/new-app/src/app/cart/cart.component.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { AuthService } from '../core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -19,17 +20,23 @@ import { AuthService } from '../core';
     COMMON,
     MatFormFieldModule,
     MatInputModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+  noteControl: FormControl;
   constructor(
     private cart: CartService,
     private location: PlatformLocation,
     private router: Router,
-    private auth: AuthService
+    private auth: AuthService,
   ) {
+    this.noteControl = new FormControl(this.cart.note());
+    this.noteControl.valueChanges.subscribe((value) => {
+      this.cart.note.set(value);
+    });
     if (this.cart.length() === 0) {
       this.location.back();
     }
