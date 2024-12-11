12b44181df86708ba09bf4f382fb99466b019b5f
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-12-11 14:48:44 +0330

    order list filter

--- apps/new-panel/src/app/orders/list/list.component.ts
+++ apps/new-panel/src/app/orders/list/list.component.ts
@@ -11,11 +11,37 @@ import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrdersService } from '../order.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
+
+const DEFAULT_STATES = [
+  OrderState.Pending,
+  OrderState.Processing,
+  OrderState.Ready,
+  OrderState.Shipping,
+  OrderState.Completed,
+  OrderState.Canceled,
+];
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, SHARED, MatToolbarModule, TableComponent, MatCardModule, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    SHARED,
+    MatToolbarModule,
+    TableComponent,
+    MatCardModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    MatChipsModule,
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -24,12 +50,18 @@ export class OrderListComponent {
   private router = inject(Router);
   private route = inject(ActivatedRoute);
   private ordersService = inject(OrdersService);
+  DEFAULT_STATES = DEFAULT_STATES;
   private queryParams = this.route.snapshot.queryParams;
   currentPage = signal(Number(this.queryParams['page'] || 0));
   currentSize = signal(Number(this.queryParams['size'] || 10));
+  OrderState = OrderState;
+  statesFilter = signal<OrderState[]>(
+    this.queryParams['states'] ? JSON.parse(this.queryParams['states']) : DEFAULT_STATES,
+  );
   filterDto = computed<FilterOrderDto>(() => ({
     take: this.currentSize(),
     skip: this.currentPage() * this.currentSize(),
+    states: this.statesFilter(),
     withCount: true,
   }));
   query = injectQuery(() => ({
@@ -48,6 +80,7 @@ export class OrderListComponent {
         queryParams: {
           page: this.currentPage(),
           size: this.currentSize(),
+          states: JSON.stringify(this.statesFilter()),
         },
       });
     });
