b7e317cc13f28f87cfbc0665df16cb981b9046d8
Mahdi Ketabdar <mahdi7192@gmail.com>
2025-03-12 17:43:18 +0330

    feat: add loading spinner to order details and refactor order fetching logic

--- apps/new-app/src/app/orders/details/details.component.ts
+++ apps/new-app/src/app/orders/details/details.component.ts
@@ -1,53 +1,52 @@
-import { Component, computed, OnDestroy, signal, WritableSignal } from '@angular/core';
+import { Component, computed, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { COMMON } from '../../common';
 import { TopAppBarComponent } from '../../common/components';
 import { MatListModule } from '@angular/material/list';
-import { Order } from '@menno/types';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrdersService } from '../../core/services/orders.service';
 import { AlertBannerComponent } from '../../common/components/alert-banner/alert-banner.component';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { lastValueFrom } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule, COMMON, TopAppBarComponent, MatListModule, AlertBannerComponent],
+  imports: [
+    CommonModule,
+    COMMON,
+    TopAppBarComponent,
+    MatListModule,
+    AlertBannerComponent,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
-export class DetailsComponent implements OnDestroy {
-  order = signal<Order | undefined>(undefined);
+export class DetailsComponent {
   shop = computed(() => {
     return this.order()?.shop;
   });
   interval: any;
 
+  orderQuery = injectQuery(() => ({
+    queryKey: ['order', this.id],
+    queryFn: () => lastValueFrom(this.ordersService.getById(this.id)),
+    refetchInterval: 30000,
+  }));
+
+  order = computed(() => {
+    return this.orderQuery.data();
+  });
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private ordersService: OrdersService,
-  ) {
-    const orderState = this.router.getCurrentNavigation()?.extras?.state?.['order'];
-    if (orderState) this.order.set(orderState);
-    else {
-      this.load();
-    }
-
-    this.interval = setInterval(() => {
-      this.load();
-    }, 20000);
-  }
-
-  async load() {
-    const order = await this.ordersService.getById(this.id).toPromise();
-    if (order) this.order.set(order);
-  }
+  ) {}
 
   get id() {
     return this.route.snapshot.params['id'];
-  }
-
-  ngOnDestroy(): void {
-    if (this.interval) clearInterval(this.interval);
   }
 }
