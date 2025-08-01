3a76dc23bf2a7d77ae4c0b0efef78f528ca7b5a0
Mahdi Ketabdar <mahdi7192@gmail.com>
2025-08-01 21:52:42 +0330

    feat: add material cost and extra costs columns to order table; implement functionality for setting extra costs and displaying profit calculations

--- apps/new-panel/src/app/orders/list/table/table.component.ts
+++ apps/new-panel/src/app/orders/list/table/table.component.ts
@@ -6,8 +6,14 @@ import { MatTableModule } from '@angular/material/table';
 import { OrderStateChipComponent } from '../../state-chip/state-chip.component';
 import { Router } from '@angular/router';
 import { ShopService } from '../../../shop/shop.service';
+import { OrdersService } from '../../order.service';
+import { FormControl } from '@angular/forms';
+import { DialogService } from '../../../core/services/dialog.service';
+import { TranslateService } from '@ngx-translate/core';
+
 const COLS = ['createdAt', 'customer', 'type', 'price', 'state', 'actions'];
 const RESTAURANT_COLS = ['createdAt', 'type', 'customer', 'price', 'state', 'actions'];
+const ALL_COLS = ['createdAt', 'customer', 'type', 'price', 'materialCost', 'extraCosts', 'profit', 'state', 'actions'];
 @Component({
   selector: 'app-order-table',
   standalone: true,
@@ -24,6 +30,9 @@ export class TableComponent {
   displayedColumns = this.shopService.isRestaurantOrCoffeeShop() ? RESTAURANT_COLS : COLS;
   User = User;
   OrderType = OrderType;
+  private readonly orderService = inject(OrdersService);
+  private readonly dialog = inject(DialogService);
+  private readonly translate = inject(TranslateService);
 
   navigateToOrder(order: Order) {
     this.router.navigate(['/orders/details', order.id], {
@@ -31,5 +40,44 @@ export class TableComponent {
         order,
       },
     });
+  }
+
+  setExtraCosts(orderId: string) {
+    this.dialog
+      .prompt(
+        this.translate.instant('order.extraCosts'),
+        {
+          value: {
+            label: this.translate.instant('order.extraCosts'),
+            control: new FormControl(0),
+            type: 'number',
+            eng: true,
+            ltr: true,
+            hint: this.translate.instant('app.currency'),
+          },
+        },
+        {
+          description: this.translate.instant('order.extraCostsDescription'),
+        },
+      )
+      .then((result) => {
+        if (result) {
+          this.orderService.setExtraCostsMutation.mutate({ orderId, extraCosts: result.value });
+        }
+      });
+  }
+
+  extraCostHint() {
+    this.dialog.alert(
+      this.translate.instant('app.unknown'),
+      this.translate.instant('order.unknownMaterialCostDescription'),
+      {
+        config: {
+          data: {
+            hideCancel: true,
+          },
+        },
+      },
+    );
   }
 }
